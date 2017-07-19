1404d397f2eba48ca2451cadb4dd74671a35a94e
vfhhu <newvfhhu@yahoo.com.tw>
2017-07-19 17:38:27 +0800

    Initial commit

---
+++ android/src/main/java/xyz/vfhhu/lib/android/MLog.java
@@ -0,0 +1,83 @@
+package xyz.vfhhu.lib.android;
+
+import android.util.Log;
+
+/**
+ * Created by leo on 2017/7/19.
+ */
+
+public class MLog {
+    private static boolean SHOW_LOG = BuildConfig.DEBUG;
+
+    public static boolean isShowLog() {
+        return SHOW_LOG;
+    }
+
+    public static void setShowLog(boolean showLog) {
+        SHOW_LOG = showLog;
+    }
+
+    private static String getClassName(Object obj) {
+        return obj.getClass().getSimpleName();
+    }
+
+    public static void v(Object obj, String msg) {
+        if (SHOW_LOG) Log.v(getClassName(obj), msg);
+    }
+
+    public static void d(Object obj, String msg) {
+        if (SHOW_LOG) Log.d(getClassName(obj), msg);
+    }
+
+    public static void i(Object obj, String msg) {
+        if (SHOW_LOG) Log.i(getClassName(obj), msg);
+    }
+
+    public static void w(Object obj, String msg) {
+        if (SHOW_LOG) Log.w(getClassName(obj), msg);
+    }
+
+    public static void e(Object obj, String msg) {
+        if (SHOW_LOG) Log.e(getClassName(obj), msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (SHOW_LOG) Log.v(tag, msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (SHOW_LOG) Log.d(tag, msg);
+    }
+
+    public static void i(String tag, String msg) {
+        if (SHOW_LOG) Log.i(tag, msg);
+    }
+
+    public static void w(String tag, String msg) {
+        if (SHOW_LOG) Log.w(tag, msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (SHOW_LOG) Log.e(tag, msg);
+    }
+
+    public static void v(Class<?> cls, String msg) {
+        if (SHOW_LOG) Log.v(cls.getSimpleName(), msg);
+    }
+
+    public static void d(Class<?> cls, String msg) {
+        if (SHOW_LOG) Log.d(cls.getSimpleName(), msg);
+    }
+
+    public static void i(Class<?> cls, String msg) {
+        if (SHOW_LOG) Log.i(cls.getSimpleName(), msg);
+    }
+
+    public static void w(Class<?> cls, String msg) {
+        if (SHOW_LOG) Log.w(cls.getSimpleName(), msg);
+    }
+
+    public static void e(Class<?> cls, String msg) {
+        if (SHOW_LOG) Log.e(cls.getSimpleName(), msg);
+    }
+}
---
+++ android/src/main/java/xyz/vfhhu/lib/android/MToast.java
@@ -0,0 +1,38 @@
+package xyz.vfhhu.lib.android;
+
+import android.app.Activity;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * Created by leo on 2017/7/19.
+ */
+
+public class MToast {
+    public static void show(final Activity act, final CharSequence text,final int duration){
+        if(Looper.getMainLooper().getThread() == Thread.currentThread()){
+            Toast.makeText(act,text, duration).show();
+            return;
+        }
+        act.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(act,text, duration).show();
+
+            }
+        });
+    }
+    public static void show(final Activity act, final int resId, final int duration){
+        if(Looper.getMainLooper().getThread() == Thread.currentThread()){
+            Toast.makeText(act,resId, duration).show();
+            return;
+        }
+        act.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(act,resId, duration).show();
+
+            }
+        });
+    }
+}
