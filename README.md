7f9b1d51bc1949af1f2908ccfd1b6121a583e719
leo <newvfhhu@yahoo.com.tw>
2019-02-15 15:16:58 +0800

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```gradle
 dependencies {
-	implementation 'xyz.vfhhu.lib:android:12.4'
+	implementation 'xyz.vfhhu.lib:android:12.5'
 	implementation 'com.squareup.okhttp3:okhttp:3.10.0'
 	implementation 'com.orhanobut:logger:2.2.0'
 }
--- android/src/main/java/xyz/vfhhu/lib/android/utils/CallBackUtil.java
+++ android/src/main/java/xyz/vfhhu/lib/android/utils/CallBackUtil.java
@@ -1,12 +1,21 @@
 package xyz.vfhhu.lib.android.utils;
 
+import java.io.UnsupportedEncodingException;
+
 /**
  * Created by leo3x on 2018/9/21.
  */
 
 public abstract class CallBackUtil<T> {
-    private String TAG;
+    private String TAG="";
     public CallBackUtil(String _TAG){TAG=_TAG;}
+    public CallBackUtil(byte[] _TAG){
+        try {
+            TAG=new String(_TAG,"UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
     public String getTAG() {
         return TAG;
     }
