ead616e72273d3ec506c7aed6c998b4dfe3e4c64
leo <newvfhhu@yahoo.com.tw>
2019-03-08 17:15:36 +0800

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ```gradle
 dependencies {
-	implementation 'xyz.vfhhu.lib:android:12.8'
+	implementation 'xyz.vfhhu.lib:android:12.9'
 	implementation 'com.squareup.okhttp3:okhttp:3.10.0'
 	implementation 'com.orhanobut:logger:2.2.0'
 }
--- android/src/main/java/xyz/vfhhu/lib/android/utils/FileUriUtil.java
+++ android/src/main/java/xyz/vfhhu/lib/android/utils/FileUriUtil.java
@@ -73,11 +73,27 @@ public class FileUriUtil {
                     path = getDataColumn(context, contentUri, selection, selectionArgs);
                     return path;
                 }
-            }
+            }else return getRealPathFromUri( context, uri);
         }
         return null;
     }
+    private static String getRealPathFromUri(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            cursor = context.getContentResolver().query(contentUri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }catch (Exception e){
+            return null;
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
 
+    }
     private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
         Cursor cursor = null;
         final String column = "_data";
